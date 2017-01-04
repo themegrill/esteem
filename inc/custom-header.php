540404d0064d5876b267b4dcf3846e20975d5651
Bishal Napit <mebishalnapit@gmail.com>
2017-01-04 11:27:26 +0545

    Added header video media support introduced in WordPress 4.7

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -19,15 +19,16 @@
  */
 function esteem_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'esteem_custom_header_args', array(
-		'default-image'          => '',
-		'default-text-color'     => '',
-		'width'                  => 1400,
-		'height'                 => 400,
-		'flex-width'			 => true,
-		'flex-height'            => true,
-		'wp-head-callback'       => '',
-		'admin-head-callback'    => '',
-		'admin-preview-callback' => 'esteem_admin_header_image',
+		'default-image'				=> '',
+		'default-text-color'		=> '',
+		'width'						=> 1400,
+		'height'					=> 400,
+		'flex-width'				=> true,
+		'flex-height'				=> true,
+		'video'						=> true,
+		'wp-head-callback'			=> '',
+		'admin-head-callback'		=> '',
+		'admin-preview-callback'	=> 'esteem_admin_header_image',
 	) ) );
 }
 add_action( 'after_setup_theme', 'esteem_custom_header_setup' );
@@ -39,11 +40,14 @@ if ( ! function_exists( 'esteem_admin_header_image' ) ) :
 function esteem_admin_header_image() {
 ?>
 	<div id="headimg">
-		<?php if ( get_header_image() ) : ?>
-		<img src="<?php header_image(); ?>" alt="<?php bloginfo( 'name' ); ?>">
-		<?php endif; ?>
+		<?php if ( function_exists( 'the_custom_header_markup' ) ) {
+			the_custom_header_markup();
+		} else {
+			if ( get_header_image() ) : ?>
+				<img src="<?php header_image(); ?>" alt="<?php bloginfo( 'name' ); ?>">
+			<?php endif;
+		} ?>
 	</div>
 <?php
 }
 endif; // esteem_admin_header_image
-?>
--- inc/header-functions.php
+++ inc/header-functions.php
@@ -8,17 +8,41 @@
  */
 
 /****************************************************************************************/
+// Filter the get_header_image_tag() for option of adding the link back to home page option
+function esteem_header_image_markup( $html, $header, $attr ) {
+	$output = '';
+	$header_image = get_header_image();
+
+	if( ! empty( $header_image ) ) {
+		$output .= '<img src="' . esc_url( $header_image ) . '" class="header-image" width="' . get_custom_header()->width . '" height="' . get_custom_header()->height . '" alt="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '">';
+	}
+
+	return $output;
+}
+
+function esteem_header_image_markup_filter() {
+	add_filter( 'get_header_image_tag', 'esteem_header_image_markup', 10, 3 );
+}
+
+add_action( 'esteem_header_image_markup_render','esteem_header_image_markup_filter' );
+
+/****************************************************************************************/
 
 if ( ! function_exists( 'esteem_render_header_image' ) ) :
 /**
  * Shows the small info text on top header part
  */
 function esteem_render_header_image() {
-	$header_image = get_header_image();
-	if( !empty( $header_image ) ) {
-	?>
-		<img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-	<?php
+	if ( function_exists( 'the_custom_header_markup' ) ) {
+		do_action( 'esteem_header_image_markup_render' );
+		the_custom_header_markup();
+	} else {
+		$header_image = get_header_image();
+		if( ! empty( $header_image ) ) {
+		?>
+			<img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+		<?php
+		}
 	}
 }
 endif;
