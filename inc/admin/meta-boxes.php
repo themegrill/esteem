151420e47d985c45f2b245efc31044006cb7a6cc
Bishal Napit <mebishalnapit@gmail.com>
2017-02-13 12:19:26 +0545

    Display the fontawesome icon select box fix for page editor section

--- inc/admin/meta-boxes.php
+++ inc/admin/meta-boxes.php
@@ -6,43 +6,43 @@
  * @subpackage Esteem
  * @since Esteem 1.0
  */
- 
+
  add_action( 'add_meta_boxes', 'esteem_add_custom_box' );
 /**
  * Add Meta Boxes.
- * 
+ *
  * Add Meta box in page and post post types.
- */ 
+ */
 function esteem_add_custom_box() {
 
 	//adds sidebar layout box in page
-	add_meta_box( 
-		'page-layout',						
-		__( 'Select Layout', 'esteem' ), 
-		'esteem_layout_call', 
-		'page', 
-		'side', 
-		'default' 
+	add_meta_box(
+		'page-layout',
+		__( 'Select Layout', 'esteem' ),
+		'esteem_layout_call',
+		'page',
+		'side',
+		'default'
 	);
 
 	//adds sidebar layout box in post
-	add_meta_box( 
+	add_meta_box(
 		'post-layout',
-		 __( 'Select Layout', 'esteem' ), 
-		 'esteem_layout_call', 
-		 'post', 
-		 'side', 
-		 'default' 
+		__( 'Select Layout', 'esteem' ),
+		'esteem_layout_call',
+		'post',
+		'side',
+		'default'
 	);
 
 	//adds fontawesome icons dropdown lists
 	add_meta_box(
-		'services-icon',							  						//Unique ID
-		__( 'Icon class', 'esteem' ),   									//Title
-		'esteem_icon_call',                   								//Callback function
-		'page',                                          					//show metabox in posts
-		'side'																//show metabox at side of editor
-	); 
+		'services-icon',					//Unique ID
+		__( 'Icon class', 'esteem' ),		//Title
+		'esteem_icon_call',					//Callback function
+		'page',								//show metabox in posts
+		'side'								//show metabox at side of editor
+	);
 
 }
 global $prefix,$esteem_metabox_field_icons,$esteem_metabox_field_layout;
@@ -56,26 +56,26 @@ $esteem_metabox_field_layout = array(
 		'type'			=>'radio',
 		'default'		=> 'default_layout',
 		'options' 		=> array (
-		    'one' => array (
-		        'label' => __( 'Default Layout', 'esteem' ),
-		        'value' => 'default_layout'
-		    ),
-		    'two' => array (
-		        'label' => __( 'Right Sidebar', 'esteem' ),
-		        'value' => 'right_sidebar'
-		    ),
-		    'three' => array (
-		        'label' => __( 'Left Sidebar', 'esteem' ),
-		        'value' => 'left_sidebar'
-		    ),
-		    'four' => array (
-		        'label' => __( 'No sidebar full width', 'esteem' ),
-		        'value' => 'no_sidebar_full_width'
-		    ),
-		    'five' => array (
-		        'label' => __( 'No sidebar content Centered', 'esteem' ),
-		        'value' => 'no_sidebar_content_centered'
-		    )
+			'one' => array (
+				'label' => __( 'Default Layout', 'esteem' ),
+				'value' => 'default_layout'
+			),
+			'two' => array (
+				'label' => __( 'Right Sidebar', 'esteem' ),
+				'value' => 'right_sidebar'
+			),
+			'three' => array (
+				'label' => __( 'Left Sidebar', 'esteem' ),
+				'value' => 'left_sidebar'
+			),
+			'four' => array (
+				'label' => __( 'No sidebar full width', 'esteem' ),
+				'value' => 'no_sidebar_full_width'
+			),
+			'five' => array (
+				'label' => __( 'No sidebar content Centered', 'esteem' ),
+				'value' => 'no_sidebar_content_centered'
+			)
 		)
 	)
 );
@@ -83,9 +83,9 @@ $esteem_metabox_field_layout = array(
 $esteem_metabox_field_icons = array(
 	array(
 		'id'			=> $prefix.'_font_icon',
-		'label' 		=> __( 'fontawesome Icons', 'esteem' ),
+		'label'			=> __( 'fontawesome Icons', 'esteem' ),
 		'type'			=> 'select',
-		'options' 		=> esteem_icon_list()
+		'options'		=> esteem_icon_list()
 	)
 );
 function esteem_layout_call() {
@@ -101,9 +101,9 @@ function esteem_icon_call() {
 /**
  * Renders the metabox
  */
-function esteem_page_layout( $esteem_metabox_field ) {  
+function esteem_page_layout( $esteem_metabox_field ) {
 	global $post;
-	//Use nonce for verification  
+	//Use nonce for verification
 	wp_nonce_field( basename( __FILE__ ), 'esteem_metabox_nonce' );
 
 	foreach ($esteem_metabox_field as $field) {
@@ -112,11 +112,11 @@ function esteem_page_layout( $esteem_metabox_field ) {
 
 			// select
 			case 'select':
-		    	echo '<select name="'.$field['id'].'" id="'.$field['id'].'" class="fontawesome">';
-		    	foreach ($field['options'] as $option_key => $option_value) {
-		        	echo '<option', $meta == $option_key ? ' selected="selected"' : '', ' value="'.$option_key.'">'.str_replace('&amp;', '&', htmlentities(stripslashes($option_value), ENT_QUOTES, "UTF-8")).'</option>';
-		    	}
-		    	echo '</select><br />';
+				echo '<select name="'.$field['id'].'" id="'.$field['id'].'" class="fontawesome" style="font-family: fontawesome;">';
+				foreach ($field['options'] as $option_key => $option_value) {
+					echo '<option', $meta == $option_key ? ' selected="selected"' : '', ' value="'.$option_key.'">'.str_replace('&amp;', '&', htmlentities(stripslashes($option_value), ENT_QUOTES, "UTF-8")).'</option>';
+				}
+				echo '</select><br />';
 
 			break;
 
@@ -125,10 +125,10 @@ function esteem_page_layout( $esteem_metabox_field ) {
 				if( empty( $meta) ){
 					$meta = $field['default'];
 				}
-			    foreach ( $field['options'] as $option ) {
-			        echo '<input type="radio" name="'.$field['id'].'" id="'.$option['value'].'" value="'.$option['value'].'" ',$meta == $option['value'] ? ' checked="checked"' : '',' />
-			                <label for="'.$option['value'].'">'.$option['label'].'</label><br />';
-			    }
+				foreach ( $field['options'] as $option ) {
+					echo '<input type="radio" name="'.$field['id'].'" id="'.$option['value'].'" value="'.$option['value'].'" ',$meta == $option['value'] ? ' checked="checked"' : '',' />
+							<label for="'.$option['value'].'">'.$option['label'].'</label><br />';
+				}
 			break;
 
 		}
@@ -137,46 +137,46 @@ function esteem_page_layout( $esteem_metabox_field ) {
 
 // Save the Data
 function esteem_save_custom_meta( $post_id ) {
-    global $esteem_metabox_field_icons, $esteem_metabox_field_layout,$post;
+	global $esteem_metabox_field_icons, $esteem_metabox_field_layout,$post;
 
-     
+
 	// Verify the nonce before proceeding.
 	if ( !isset( $_POST[ 'esteem_metabox_nonce' ] ) || !wp_verify_nonce( $_POST[ 'esteem_metabox_nonce' ], basename( __FILE__ ) ) )
-    	return;
+		return;
 
-    // check autosave
-    if ( defined('DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
-    	return $post_id;
+	// check autosave
+	if ( defined('DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+		return $post_id;
 
-    // check permissions
-    if ( 'page' == $_POST['post_type'] ) {
-    	if ( !current_user_can( 'edit_page', $post_id ) )
-    		return $post_id;
-    }
-     
-    // loop through fields and save the data
-    foreach ( $esteem_metabox_field_icons as $field ) {
-    	$old = get_post_meta( $post_id, $field['id'], true );
-        $new = $_POST[$field['id']];
-        update_post_meta( $post_id,$field['id'],$new );
-        if ($new && $new != $old) {
-            
-        } elseif ('' == $new && $old) {
-        	delete_post_meta($post_id, $field['id'], $old);
-        }
-    } // end foreach
+	// check permissions
+	if ( 'page' == $_POST['post_type'] ) {
+		if ( !current_user_can( 'edit_page', $post_id ) )
+			return $post_id;
+	}
 
-     // loop through fields and save the data
-    foreach ( $esteem_metabox_field_layout as $field ) {
-    	$old = get_post_meta( $post_id, $field['id'], true );
-        $new = $_POST[$field['id']];
-        update_post_meta( $post_id,$field['id'],$new );
-        if ($new && $new != $old) {
-            
-        } elseif ('' == $new && $old) {
-        	delete_post_meta($post_id, $field['id'], $old);
-        }
-    } // end foreach
+	// loop through fields and save the data
+	foreach ( $esteem_metabox_field_icons as $field ) {
+		$old = get_post_meta( $post_id, $field['id'], true );
+		$new = $_POST[$field['id']];
+		update_post_meta( $post_id,$field['id'],$new );
+		if ($new && $new != $old) {
+
+		} elseif ('' == $new && $old) {
+			delete_post_meta($post_id, $field['id'], $old);
+		}
+	} // end foreach
+
+	// loop through fields and save the data
+	foreach ( $esteem_metabox_field_layout as $field ) {
+		$old = get_post_meta( $post_id, $field['id'], true );
+		$new = $_POST[$field['id']];
+		update_post_meta( $post_id,$field['id'],$new );
+		if ($new && $new != $old) {
+
+		} elseif ('' == $new && $old) {
+			delete_post_meta($post_id, $field['id'], $old);
+		}
+	} // end foreach
 }
 add_action('pre_post_update', 'esteem_save_custom_meta');
 ?>
