f2f597a014abfd91fc34842ef3f2585ffaaa6c72
Ashish Shrestha <ashishsthanp@gmail.comgio>
2017-06-21 15:11:45 +0545

    Fixed enqueue handle

--- inc/functions.php
+++ inc/functions.php
@@ -38,7 +38,7 @@ function esteem_scripts_styles_method() {
 	/**
 	 * Register JQuery cycle 2 js file for slider.
 	 */
-	wp_register_script( 'jquery-cycle', ESTEEM_JS_URL . '/jquery.cycle2.min.js', array( 'jquery' ), '2.1.6', true );
+	wp_register_script( 'jquery_cycle', ESTEEM_JS_URL . '/jquery.cycle2.min.js', array( 'jquery' ), '2.1.6', true );
 	wp_register_script( 'jquery-cycle2-swipe', ESTEEM_JS_URL . '/jquery.cycle2.swipe.min.js', array( 'jquery' ), false, true );
 
 	/**
@@ -46,7 +46,7 @@ function esteem_scripts_styles_method() {
 	 */
 	if( get_theme_mod( 'esteem_activate_slider', '0' ) == '1' ) {
 		if ( is_home() || is_front_page() ) {
-			wp_enqueue_script( 'esteem-slider', ESTEEM_JS_URL . '/esteem-slider-setting.js', array( 'jquery-cycle', 'jquery-cycle2-swipe' ), false, true );
+			wp_enqueue_script( 'esteem-slider', ESTEEM_JS_URL . '/esteem-slider-setting.js', array( 'jquery_cycle', 'jquery-cycle2-swipe' ), false, true );
 
 		}
 	}
