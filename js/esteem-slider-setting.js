1fea7c94fa9922507d3676393d10889f86c7dc4c
Ashish Shrestha <ashishsthanp@gmail.comgio>
2017-06-21 13:04:30 +0545

    Upgraded jQuery cycle for slider to jQuery cycle 2 with swipe

--- js/esteem-slider-setting.js
+++ js/esteem-slider-setting.js
@@ -5,22 +5,27 @@
  */
 
 jQuery(window).load(function() {
-jQuery('.slider-cycle').cycle({
-	fx:					'fade',
-	pager:				'#controllers',
-	activePagerClass: 	'active',
-	timeout:			4000,
-	speed:				1000,
-	pause:				1,
-	pauseOnPagerHover:	1,
-	width:				'100%',
-	containerResize:	0,
-	fit:				1,
-	after:				function ()	{
-							jQuery(this).parent().css('height', jQuery(this).height());
-						},
-	cleartypeNoBg: 		true
+	jQuery('.slider-cycle').cycle({
+		fx:					'fade',
+		slides:				'> section',
+		pager:				'> #controllers',
+		pagerActiveClass: 	'active',
+		pagerTemplate:		'<a></a>',
+		timeout:			4000,
+		speed:				1000,
+		pause:				1,
+		pauseOnPagerHover:	1,
+		width:				'100%',
+		containerResize:	0,
+		autoHeight:			'container',
+		fit:				1,
+		after:				function ()	{
+								jQuery(this).parent().css('height', jQuery(this).height());
+							},
+		cleartypeNoBg: 		true,
+		log:				false,
+		swipe:				true
 
-});
+	});
 
 });
