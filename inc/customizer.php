6fd58b7225c5e5ae6ee0562da4c9ba254060ac26
Nima <nima.themegrill@gmail.com>
2017-08-29 16:49:59 +0545

    Tweaks - Introduce selective refresh for site title and description

--- inc/customizer.php
+++ inc/customizer.php
@@ -7,6 +7,20 @@
  * @since Esteem 1.2.7
  */
 function esteem_customize_register($wp_customize) {
+   $wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+   $wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+
+   if ( isset( $wp_customize->selective_refresh ) ) {
+     $wp_customize->selective_refresh->add_partial( 'blogname', array(
+        'selector'        => '#site-title a',
+        'render_callback' => 'esteem_customize_partial_blogname',
+     ) );
+
+     $wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+        'selector'        => '.site-description',
+        'render_callback' => 'esteem_customize_partial_blogdescription',
+     ) );
+   }
 
 	// Theme important links started
    class Esteem_Important_Links extends WP_Customize_Control {
@@ -658,6 +672,24 @@ function esteem_customize_register($wp_customize) {
    }
 }
 add_action('customize_register', 'esteem_customize_register');
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function esteem_customize_partial_blogname() {
+   bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function esteem_customize_partial_blogdescription() {
+   bloginfo( 'description' );
+}
 
 /*****************************************************************************************/
 
