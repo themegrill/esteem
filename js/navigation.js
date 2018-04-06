a4f8f35abfa1eb357ea3e615759cafeed85f6e59
Ashish Shrestha <ashishsthanp@gmail.com>
2018-04-06 16:22:47 +0545

    Fix - sub menu caret toggle

--- js/navigation.js
+++ js/navigation.js
@@ -36,11 +36,19 @@
 		}
 	};
 
-	    jQuery('#site-navigation .menu-item-has-children').append('<span class="sub-toggle"> <i class="icon-caret-down"></i> </span>');
-    jQuery('#site-navigation .sub-toggle').click(function() {
-        jQuery(this).parent('.menu-item-has-children').children('ul.sub-menu').first().slideToggle('1000');
-        jQuery(this).children('.icon-caret-right').first().toggleClass('icon-caret-down');
-        jQuery(this).toggleClass('active');
-    });
 } )();
 
+jQuery( document ).ready( function () {
+	/**
+	 * New responsive sub menu toggle
+	 */
+	// Add caret icon
+	jQuery( '.better-responsive-menu #site-navigation .menu-item-has-children' ).append( '<span class="sub-toggle"> <i class="icon-caret-down"></i> </span>' );
+
+	// Handle clicking on caret
+	jQuery( '.better-responsive-menu #site-navigation .sub-toggle' ).click( function () {
+		jQuery( this ).parent( '.menu-item-has-children' ).children( 'ul.sub-menu' ).first().slideToggle( '1000' );
+		jQuery( this ).children( '.icon-caret-down' ).first().toggleClass( 'icon-caret-up' );
+		jQuery( this ).toggleClass( 'active' );
+	} );
+} );
