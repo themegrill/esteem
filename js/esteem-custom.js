1cd989852a7d27b3cf4f67a0885a9dbf41d7ba38
Ashish Shrestha <ashishsthanp@gmail.com>
2018-04-10 16:46:26 +0545

    Code reformat

--- js/esteem-custom.js
+++ js/esteem-custom.js
@@ -1,34 +1,36 @@
-jQuery(document).ready(function(){
-	jQuery('.search-top').click(function(){
-		jQuery('#masthead .search-form-top').toggle();
-	});
-});
+jQuery( document ).ready( function () {
+	jQuery( '.search-top' ).click( function () {
+		jQuery( '#masthead .search-form-top' ).toggle();
+	} );
 
-jQuery(document).ready(function(){
-	jQuery('#scroll-up').hide();
-	jQuery(function () {
-		jQuery(window).scroll(function () {
-			if (jQuery(this).scrollTop() > 1000) {
-				jQuery('#scroll-up').fadeIn();
+	jQuery( '#scroll-up' ).hide();
+
+	jQuery( function () {
+		jQuery( window ).scroll( function () {
+			if ( jQuery( this ).scrollTop() > 1000 ) {
+				jQuery( '#scroll-up' ).fadeIn();
 			} else {
-				jQuery('#scroll-up').fadeOut();
+				jQuery( '#scroll-up' ).fadeOut();
 			}
-		});
-		jQuery('a#scroll-up').click(function () {
-			jQuery('body,html').animate({
+		} );
+
+		jQuery( 'a#scroll-up' ).click( function () {
+			jQuery( 'body,html' ).animate( {
 				scrollTop: 0
-			}, 800);
+			}, 800 );
 			return false;
-		});
-	});
-});
-jQuery(document).on('click', '#site-navigation .menunav-menu li.menu-item-has-children > a', function(event) {
-    var menuClass = jQuery(this).parent('.menu-item-has-children');
-    if (! menuClass.hasClass('focus')){
-        menuClass.addClass('focus');
-        event.preventDefault();
-        menuClass.children('.sub-menu').css({
-           'display': 'block'
-        });
-    }
-  });
+		} );
+	} );
+} );
+
+jQuery( document ).on( 'click', '#site-navigation .menunav-menu li.menu-item-has-children > a', function ( event ) {
+	var menuClass = jQuery( this ).parent( '.menu-item-has-children' );
+
+	if ( !menuClass.hasClass( 'focus' ) ) {
+		menuClass.addClass( 'focus' );
+		event.preventDefault();
+		menuClass.children( '.sub-menu' ).css( {
+			'display': 'block'
+		} );
+	}
+} );
