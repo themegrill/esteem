42da2f4404229edd78ebb0aa168983f82896570c
Bishal Napit <mebishalnapit@gmail.com>
2019-08-13 16:34:14 +0545

    Added - Skip to content link

--- header.php
+++ header.php
@@ -29,6 +29,8 @@ if ( function_exists( 'wp_body_open' ) ) {
 ?>
 
 <div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'esteem' ); ?></a>
+
 	<header id="masthead" class="site-header" role="banner">
 		<div class="inner-wrap">
 			<div class="hgroup-wrap clearfix">
